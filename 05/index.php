2fb48452a30dfd3cac8d8ad43abee2058d8fc656
inunosinsi <tsuyoshi@saitodev.co>
2023-02-14 14:32:08 +0900

    [Add]Added SOY2Session sample code.

---
+++ 05/index.php
@@ -0,0 +1,25 @@
+<?php
+define("WEBAPP_DIR", dirname(__FILE__) . "/webapp/");
+
+//include SOY2
+if(!class_exists("SOY2")) include_once(WEBAPP_DIR . "lib/soy2_build.php");
+
+// SOY2HTMLで出力するクラスファイルの格納場所を指定
+SOY2HTMLConfig::PageDir(WEBAPP_DIR . "pages/");
+
+// SOY2HTMLで出力したページのキャッシュファイルの格納場所を指定
+SOY2HTMLConfig::CacheDir(WEBAPP_DIR . "cache/");
+
+// SOY2HTMLConfig::PageDirで指定したディレクトリからページのクラスファイルを読み込む
+$page = SOY2HTMLFactory::createInstance("InputPage");
+
+// TopPage.class.php + TopPage.htmlから出力用のHTMLを組み立てる
+ob_start();
+$page->display();
+$html = ob_get_contents();
+ob_end_clean();
+
+include(WEBAPP_DIR."layout/main.php");
+exit;
+
+//echo $html;
