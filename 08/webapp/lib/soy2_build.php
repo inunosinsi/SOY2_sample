1e19740a2bb686ccbcaeaad9a7f7c5c56c5cac05
inunosinsi <tsuyoshi@saitodev.co>
2023-02-18 17:34:47 +0900

    [Add]Added accept attribute in HTMLUploadForm.

--- 08/webapp/lib/soy2_build.php
+++ 08/webapp/lib/soy2_build.php
@@ -6065,14 +6065,19 @@ class HTMLCSSLink extends SOY2HTML{
  	function setDisabled($disabled) {
  		$this->disabled = $disabled;
  	}
- }
+}
 /**
  * @package SOY2.SOY2HTML
  */
 class HTMLUploadForm extends HTMLForm{
+
+	private $accept;
+
 	function execute(){
 		parent::execute();
 		$this->setAttribute("enctype","multipart/form-data");
+		$accept = (is_string($this->accept)) ? trim($this->accept) : "";
+		$this->setAttribute("accept", $accept, false);
 	}
 }
 /**
@@ -6085,6 +6090,7 @@ abstract class HTMLFormElement extends SOY2HTML{
 	private $required;
 	private $placeholder;
 	private $pattern;
+	private $autocomplete;
 	function execute(){
 		parent::execute();
 		$disabled = (is_string($this->disabled) || (is_bool($this->disabled)) && $this->disabled) ? "disabled" : "";
@@ -6097,6 +6103,8 @@ abstract class HTMLFormElement extends SOY2HTML{
 		$this->setAttribute("placeholder", $placeholder, false);
 		$pattern = (is_string($this->pattern)) ? trim($this->pattern) : "";
 		$this->setAttribute("pattern", $pattern, false);
+		$autocomplete = (is_string($this->autocomplete)) ? trim($this->autocomplete) : "";
+		$this->setAttribute("autocomplete", $autocomplete, false);
 	}
 	function setName($value){
 		$this->name = $value;
@@ -6131,6 +6139,12 @@ abstract class HTMLFormElement extends SOY2HTML{
 	}
 	function setPattern($pattern){
 		$this->pattern = $pattern;
+	}
+	function getAutocomplete(){
+		return $this->autocomplete;
+	}
+	function setAutocomplete($autocomplete){
+		$this->autocomplete = $autocomplete;
 	}
 }
 /**
