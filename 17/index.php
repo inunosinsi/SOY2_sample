207536bf22d0b0a7a7e29b9c687e51f558ebe950
inunosinsi <tsuyoshi@saitodev.co>
2023-02-21 15:55:13 +0900

    [Add]Added SOY2DAO sample code.

---
+++ 17/index.php
@@ -0,0 +1,31 @@
+<?php
+//include SOY2
+define("WEBAPP_DIR", dirname(__FILE__) . "/webapp/");
+if(!class_exists("SOY2")) include_once(WEBAPP_DIR . "lib/soy2_build.php");
+
+//SQLiteのインメモリを使用
+SOY2DAOConfig::Dsn("sqlite::memory:");
+SOY2DAOConfig::DaoCacheDir(WEBAPP_DIR . "/cache/");
+
+$dao = new SOY2DAO();
+$sql = "CREATE TABLE soy2_sample (id integer primary key AUTOINCREMENT, text TEXT);";
+try{
+	$dao->executeQuery($sql);
+}catch(Exception $e){
+	var_dump($e);
+}
+
+$sql = "INSERT INTO soy2_sample (text) VALUES('hoge');";
+try{
+	$dao->executeQuery($sql);
+}catch(Exception $e){
+	var_dump($e);
+}
+
+$sql = "SELECT * FROM soy2_sample;";
+try{
+	$results = $dao->executeQuery($sql);
+	var_dump($results);
+}catch(Exception $e){
+	var_dump($e);
+}
