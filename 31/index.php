abf1b0597abfb85ab5b9f355e4f724993ab7b224
inunosinsi <tsuyoshi@saitodev.co>
2023-03-01 15:46:35 +0900

    [Add]Added sample code for a method that executes a self-made query.

---
+++ 31/index.php
@@ -0,0 +1,40 @@
+<?php
+include("config.php");
+
+$dao = new SOY2DAO();
+
+// テーブルスキーマが記述されているファイルを取得する
+$sql = file_get_contents(SOY2::RootDir() . "sql/init_sqlite.sql");
+$dao->executeQuery($sql);
+
+// データベースにisOpenがSOY2Sample::IS_OPENとSOY2Sample::NO_OPENのレコードを交互に挿入する
+$fruits = array(
+	"apple",
+	"orange",
+	"melon",
+	"peach",
+	"grape"
+);
+
+$dao = SOY2DAOFactory::create("SOY2SampleDAO");
+foreach($fruits as $idx => $f){
+	$obj = new SOY2Sample();
+	$obj->setText($f);
+
+	if($idx % 2 === 0){
+		$obj->setIsOpen(SOY2Sample::IS_OPEN);
+	} else {
+		$obj->setIsOpen(SOY2Sample::NO_OPEN);
+	}
+
+	try{
+		$id = $dao->insert($obj);
+	}catch(Exception $e){
+		//
+	}
+}
+
+// getOpenStatusList内でtry-catchしているので、この場では不要
+$list = $dao->getOpenStatusList();
+
+var_dump($list);
---
+++ 31/webapp/src/domain/SOY2SampleDAO.class.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @entity SOY2Sample
+ */
+abstract class SOY2SampleDAO extends SOY2DAO {
+
+	/**
+	 * @return id
+	 * @trigger onInsert
+	 */
+	abstract function insert(SOY2Sample $bean);
+
+	/**
+	 * @trigger onUpdate
+	 */
+	abstract function update(SOY2Sample $bean);
+
+	/**
+	 * @return list
+	 */
+	abstract function get();
+
+	/**
+	 * @return object
+	 */
+	abstract function getById(int $id);
+
+	/**
+	 * @return list
+	 */
+	abstract function getByIsOpen(int $isOpen);
+
+	/**
+	 * @columns count(*) as object_count
+	 * @return column_object_count
+	 */
+	abstract function count();
+
+	/**
+	 * @columns count(*) as object_count
+	 * @return column_object_count
+	 */
+	abstract function countByIsOpen(int $isOpen);
+
+	/**
+	 * 公開しているオブジェクトと公開していないオブジェクトの各々の件数を取得する
+	 * @return array
+	 */
+	function getOpenStatusList(){
+		$sql = "SELECT is_open, COUNT(is_open) AS count_is_open FROM soy2_sample GROUP BY is_open";
+		try{
+			$results = $this->executeQuery($sql);
+		}catch(Exception $e){
+			$results = array();
+		}
+		if(!count($results)) return array();
+		
+		$list = array();
+		foreach($results as $res){
+			$list[$res["is_open"]] = $res["count_is_open"];
+		}
+		return $list;
+	}
+
+	/**
+	 * @final
+	 */
+	function onInsert(SOY2DAO_Query $query, array $binds){
+		$binds[":createDate"] = time();
+		$binds[":updateDate"] = time();
+		return array($query, $binds);
+	}
+
+	/**
+	 * @final
+	 */
+	function onUpdate(SOY2DAO_Query $query, array $binds){
+		$binds[":updateDate"] = time();
+		return array($query, $binds);
+	}
+}
